c89c10bbb24b909a2e0cf991379fdb1e08fc95a9
Mike Manchee <mikemanchee@gmail.com>
2020-10-09 09:06:23 -0700

    add SQL DB and Stylist Clotroller Index() to the project

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,7 +4,10 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public string ClientName { get; set; }
     public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
+  
 }
